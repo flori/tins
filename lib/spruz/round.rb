234dd209c2b27edf37aa26a014c389cbe3e2e67b
Florian Frank <flori@ping.de>
2010-05-18 12:23:10 +0200

    made round method more compatible to ruby method
    this actually calls the ruby-native method if places is 0 or nil

--- lib/spruz/round.rb
+++ lib/spruz/round.rb
@@ -5,6 +5,7 @@ module Spruz
       if klass.instance_method(:round)
         klass.class_eval do
           begin
+            alias_method :__old_round__, :round
             remove_method :round
           rescue NameError
           end
@@ -15,9 +16,13 @@ module Spruz
       end
     end
 
-    def round(places = 0)
-      unless Integer === places
-        raise TypeError, "argument places has to be an Integer"
+    def round(places = nil)
+      if places == nil || places == 0
+        return __old_round__
+      elsif places.respond_to?(:to_int)
+        places = places.to_int
+      else
+        raise TypeError, "argument places has to be like an Integer"
       end
       if places < 0
         max_places = -Math.log(self.abs + 1) / Math.log(10)
@@ -26,13 +31,19 @@ module Spruz
       t = self
       f = 10.0 ** places
       t *= f
-      if t >= 0.0
-        t = (t + 0.5).floor
-      elsif t < 0.0
-        t = (t - 0.5).ceil
+      if t.infinite?
+        result = self
+      else
+        if t >= 0.0
+          t = (t + 0.5).floor
+        elsif t < 0.0
+          t = (t - 0.5).ceil
+        end
+        t /= f
+        result = t.nan? ? self : t
       end
-      t /= f
-      t.nan? ? self : t
+      max_places and result = result.to_i # if places < 0
+      result
     end
   end
 end
