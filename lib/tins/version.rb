17cf80ea25bd4f321155b6c819589e6b0de365dc
Florian Frank <flori@ping.de>
2011-09-27 12:00:53 +0200

    prepare a new version

--- lib/tins/version.rb
+++ lib/tins/version.rb
@@ -1,10 +1,8 @@
 module Tins
   # Tins version
-  VERSION         = '0.3.0'
+  VERSION         = '0.3.1'
   VERSION_ARRAY   = VERSION.split(/\./).map { |x| x.to_i } # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
   VERSION_BUILD   = VERSION_ARRAY[2] # :nodoc:
 end
-
-require 'tins/alias'
