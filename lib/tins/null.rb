f0ce61ee6eabb6c55f30ffdc493e69fd0e91d0bc
Florian Frank <flori@ping.de>
2011-12-25 13:16:45 +0100

    Add some more methods to null object

--- lib/tins/null.rb
+++ lib/tins/null.rb
@@ -13,6 +13,18 @@ module Tins
       ''
     end
 
+    def to_f
+      0.0
+    end
+
+    def to_i
+      0
+    end
+
+    def to_a
+      []
+    end
+
     def inspect
       'NULL'
     end
--- tests/null_test.rb
+++ tests/null_test.rb
@@ -10,6 +10,9 @@ module Tins
       assert_equal NULL, NULL.foo.bar
       assert_equal 'NULL', NULL.inspect
       assert_equal '', NULL.to_s
+      assert_equal 0, NULL.to_i
+      assert_equal 0.0, NULL.to_f
+      assert_equal [], NULL.to_a
       assert_equal 1, Null(1)
       assert_equal NULL, Null(nil)
       assert_equal NULL, NULL::NULL
