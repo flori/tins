083b076eae526e97f81d33618be84509d1103454
Florian Frank <flori@ping.de>
2012-03-09 11:48:26 +0100

    Fix test for some implementations

--- tests/subhash_test.rb
+++ tests/subhash_test.rb
@@ -9,7 +9,7 @@ module Tins
       h = { 'foo1' => 1, 'foo2' => 2, 'bar666' => 666 }
       assert_equal [ [ 'bar666', 666 ] ], h.subhash(/\Abar/).to_a
       assert h.subhash(/\Abaz/).empty?
-      assert_equal [ [ 'foo1', 1 ], [ 'foo2', 2 ] ], h.subhash(/\Afoo\d/).to_a
+      assert_equal [ [ 'foo1', 1 ], [ 'foo2', 2 ] ], h.subhash(/\Afoo\d/).sort
       assert_equal [ [ 'foo2', 2 ] ], h.subhash('foo2').to_a
     end
 
@@ -23,7 +23,7 @@ module Tins
       h = { 'foo1' => 1, 'foo2' => 2, 'bar666' => 666 }
       assert h.subhash(/\Abaz/) { :foo }.empty?
       assert_equal [ [ 'foo1', 1 ], [ 'foo2', 2 ] ],
-        h.subhash(/\Afoo(\d)/) { |_,_,m| Integer(m[1]) }.to_a
+        h.subhash(/\Afoo(\d)/) { |_,_,m| Integer(m[1]) }.to_a.sort
     end
   end
 end
