8ebf8c48db3fa3f1f5db054d766e741032ea6864
Florian Frank <flori@ping.de>
2013-02-04 13:15:23 +0100

    make things 1.8.x compatible :-(

--- tests/proc_prelude_test.rb
+++ tests/proc_prelude_test.rb
@@ -5,7 +5,7 @@ module Tins
   class ProcPreludeTest < Test::Unit::TestCase
     def test_apply
       assert_equal 42,
-        Proc.apply { |x, y, z|  x ** 2 + y ** 3 + z ** 5 }.([3, 1, 2])
+        Proc.apply { |x, y, z|  x ** 2 + y ** 3 + z ** 5 }.call([3, 1, 2])
     end
 
     class Person
@@ -32,42 +32,42 @@ module Tins
     end
 
     def test_array
-      assert_equal [1,2,3], Proc.array.(1,2,3)
+      assert_equal [1,2,3], Proc.array.call(1,2,3)
     end
 
     def test_first
-      assert_equal 1, Proc.first.(1,2,3)
-      assert_equal 1, Proc.head.(1,2,3)
+      assert_equal 1, Proc.first.call(1,2,3)
+      assert_equal 1, Proc.head.call(1,2,3)
     end
 
     def test_second
-      assert_equal 2, Proc.second.(1,2,3)
+      assert_equal 2, Proc.second.call(1,2,3)
     end
 
     def test_tail
-      assert_equal [ 2, 3 ], Proc.tail.(1,2,3)
+      assert_equal [ 2, 3 ], Proc.tail.call(1,2,3)
     end
 
     def test_last
-      assert_equal 3, Proc.last.(1,2,3)
+      assert_equal 3, Proc.last.call(1,2,3)
     end
 
     def test_rotate
-      assert_equal [ 2, 3, 1 ], Proc.rotate.(1,2,3)
-      assert_equal [ 2, 1 ], Proc.swap.(1,2)
+      assert_equal [ 2, 3, 1 ], Proc.rotate.call(1,2,3)
+      assert_equal [ 2, 1 ], Proc.swap.call(1,2)
     end
 
     def test_id1
-      assert_equal :foo, Proc.id1.(:foo)
+      assert_equal :foo, Proc.id1.call(:foo)
     end
 
     def test_const
-      assert_equal :baz, Proc.const(:baz).(:foo, :bar)
-      assert_equal :baz, Proc.const { :baz }.(:foo, :bar)
+      assert_equal :baz, Proc.const(:baz).call(:foo, :bar)
+      assert_equal :baz, Proc.const { :baz }.call(:foo, :bar)
     end
 
     def test_nth
-      assert_equal 2, Proc.nth(1).(1,2,3)
+      assert_equal 2, Proc.nth(1).call(1,2,3)
     end
   end
 end
