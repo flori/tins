e79c20601e839bc657b1c8af9de178485ca98d49
Florian Frank <flori@ping.de>
2013-06-17 18:28:33 +0200

    Skip locking and pray instead…
    Using the mutex caused recursive locking excptions, great.

--- lib/tins/sexy_singleton.rb
+++ lib/tins/sexy_singleton.rb
@@ -15,43 +15,21 @@ module Tins
     def included(klass)
       __old_singleton_included__(klass)
       (class << klass; self; end).class_eval do
-        def sexy_singleton_mutex_synchronize
-          if @singleton__mutex__
-            @singleton__mutex__.synchronize do
-              yield
-            end
-          else
-            begin
-              old_critical, Thread.critical = Thread.critical, true
-              result = yield
-            ensure
-              Thread.critical = old_critical
-            end
-            result
-          end
-        end
-
         if Object.method_defined?(:respond_to_missing?)
           def  respond_to_missing?(name, *args)
-            sexy_singleton_mutex_synchronize do
-              instance.respond_to?(name) || super
-            end
+            instance.respond_to?(name) || super
           end
         else
           def respond_to?(name, *args)
-            sexy_singleton_mutex_synchronize do
-              instance.respond_to?(name) || super
-            end
+            instance.respond_to?(name) || super
           end
         end
 
         def method_missing(name, *args, &block)
-          sexy_singleton_mutex_synchronize do
-            if instance.respond_to?(name)
-              instance.__send__(name, *args, &block)
-            else
-              super
-            end
+          if instance.respond_to?(name)
+            instance.__send__(name, *args, &block)
+          else
+            super
           end
         end
       end
