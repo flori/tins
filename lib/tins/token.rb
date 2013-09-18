4d20e7e141be050cb51cbb2c228720d7af176f1e
Florian Frank <flori@ping.de>
2013-09-19 00:58:48 +0200

    Make 1.8 and 1.9 compatible, sigh…

--- lib/tins/token.rb
+++ lib/tins/token.rb
@@ -2,7 +2,11 @@ module Tins
   class Token < String
     DEFAULT_ALPHABET = ((?0..?9).to_a + (?a..?z).to_a + (?A..?Z).to_a).freeze
 
-    def initialize(bits: 128, length: nil, alphabet: DEFAULT_ALPHABET)
+    #def initialize(bits: 128, length: nil, alphabet: DEFAULT_ALPHABET)
+    def initialize(options = {})
+      bits     = options[:bits] || 128
+      length   = options[:length]
+      alphabet = options[:alphabet] || DEFAULT_ALPHABET
       alphabet.size > 1 or raise ArgumentError, 'need at least 2 symbols in alphabet'
       if length
         length > 0 or raise ArgumentError, 'length has to be positive'
