f35a011ec26d38328658010f9e7eaefd9e02c6b1
Florian Frank <flori@ping.de>
2014-05-23 20:01:52 +0200

    Actually test the if predicate

--- tests/if_predicate_test.rb
+++ tests/if_predicate_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 require 'tins/xt'
 
 module Tins
-  class IfPredicateTest
+  class IfPredicateTest < Test::Unit::TestCase
     def test_if_predicate
       assert_equal :foo, true.if? && :foo
       assert_nil false.if? && :foo
