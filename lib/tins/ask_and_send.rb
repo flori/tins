7d6afb3362616258910d8fe6881172e430fd2f5b
Florian Frank <flori@ping.de>
2014-06-14 15:56:35 +0200

    Adds ask_and_send_or_self methods

--- lib/tins/ask_and_send.rb
+++ lib/tins/ask_and_send.rb
@@ -11,5 +11,21 @@ module Tins
         __send__(method_name, *args, &block)
       end
     end
+
+    def ask_and_send_or_self(method_name, *args, &block)
+      if respond_to?(method_name)
+        __send__(method_name, *args, &block)
+      else
+        self
+      end
+    end
+
+    def ask_and_send_or_self!(method_name, *args, &block)
+      if respond_to?(method_name, true)
+        __send__(method_name, *args, &block)
+      else
+        self
+      end
+    end
   end
 end
--- tests/ask_and_send_test.rb
+++ tests/ask_and_send_test.rb
@@ -28,5 +28,19 @@ module Tins
       assert_equal :bar, A.new.ask_and_send!(:bar)
       assert_nil A.new.ask_and_send(:baz)
     end
+
+    def test_asking_selfy_publicly
+      a = A.new
+      assert_equal :foo, a.ask_and_send_or_self(:foo)
+      assert_equal a, a.ask_and_send_or_self(:bar)
+      assert_equal a, a.ask_and_send_or_self(:baz)
+    end
+
+    def test_asking_selfy_privately
+      a = A.new
+      assert_equal :foo, a.ask_and_send_or_self!(:foo)
+      assert_equal :bar, a.ask_and_send_or_self!(:bar)
+      assert_equal a, a.ask_and_send_or_self(:baz)
+    end
   end
 end
