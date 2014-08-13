e0b7ee60aa66fd799a5fb5319a03c97e3d467342
Florian Frank <flori@ping.de>
2014-08-13 12:05:10 +0200

    Use Ruby's SecureRandom PRNG in Tins::Token

--- lib/tins/token.rb
+++ lib/tins/token.rb
@@ -1,3 +1,5 @@
+require 'securerandom'
+
 module Tins
   class Token < String
     DEFAULT_ALPHABET = ((?0..?9).to_a + (?a..?z).to_a + (?A..?Z).to_a).freeze
@@ -7,6 +9,7 @@ module Tins
       bits     = options[:bits] || 128
       length   = options[:length]
       alphabet = options[:alphabet] || DEFAULT_ALPHABET
+      random   = options[:random] || SecureRandom
       alphabet.size > 1 or raise ArgumentError, 'need at least 2 symbols in alphabet'
       if length
         length > 0 or raise ArgumentError, 'length has to be positive'
@@ -16,7 +19,7 @@ module Tins
       end
       self.bits = (Math.log(alphabet.size ** length) / Math.log(2)).floor
       token = ''
-      length.times { token << alphabet[rand(alphabet.size)] }
+      length.times { token << alphabet[random.random_number(alphabet.size)] }
       super token
     end
 
