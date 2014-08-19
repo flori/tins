3941ba480a9707409f04cac6ee67c89f64745370
Florian Frank <flori@ping.de>
2014-08-19 17:00:26 +0200

    Retain visibility of methods after memoizing

--- lib/tins/memoize.rb
+++ lib/tins/memoize.rb
@@ -13,6 +13,20 @@ module Tins
          __memoize_cache__.clear
         self
       end
+
+      def memoize_apply_visibility(id)
+        visibility = instance_eval do
+          case
+          when private_method_defined?(id)
+            :private
+          when protected_method_defined?(id)
+            :protected
+          end
+        end
+        yield
+      ensure
+        visibility and __send__(visibility, id)
+      end
     end
 
     class ::Module
@@ -25,17 +39,19 @@ module Tins
         include CacheMethods
         method_ids.each do |method_id|
           method_id = method_id.to_s.to_sym
-          orig_method = instance_method(method_id)
-          __send__(:define_method, method_id) do |*args|
-            mc = __memoize_cache__
-            if mc.key?(method_id) and result = mc[method_id][args]
+          memoize_apply_visibility method_id do
+            orig_method = instance_method(method_id)
+            __send__(:define_method, method_id) do |*args|
+              mc = __memoize_cache__
+              if mc.key?(method_id) and result = mc[method_id][args]
+                result
+              else
+                (mc[method_id] ||= {})[args] = result = orig_method.bind(self).call(*args)
+                $DEBUG and warn "#{self.class} cached method #{method_id}(#{args.inspect unless args.empty?}) = #{result.inspect} [#{__id__}]"
+                opts[:freeze] and result.freeze
+              end
               result
-            else
-              (mc[method_id] ||= {})[args] = result = orig_method.bind(self).call(*args)
-              $DEBUG and warn "#{self.class} cached method #{method_id}(#{args.inspect unless args.empty?}) = #{result.inspect} [#{__id__}]"
-              opts[:freeze] and result.freeze
             end
-            result
           end
         end
         method_ids.size == 1 ? method_ids.first : method_ids
@@ -52,16 +68,18 @@ module Tins
         mc = __memoize_cache__
         function_ids.each do |function_id|
           function_id = function_id.to_s.to_sym
-          orig_function = instance_method(function_id)
-          __send__(:define_method, function_id) do |*args|
-            if mc.key?(function_id) and result = mc[function_id][args]
+          memoize_apply_visibility function_id do
+            orig_function = instance_method(function_id)
+            __send__(:define_method, function_id) do |*args|
+              if mc.key?(function_id) and result = mc[function_id][args]
+                result
+              else
+                (mc[function_id] ||= {})[args] = result = orig_function.bind(self).call(*args)
+                opts[:freeze] and result.freeze
+                $DEBUG and warn "#{self.class} cached function #{function_id}(#{args.inspect unless args.empty?}) = #{result.inspect}"
+              end
               result
-            else
-              (mc[function_id] ||= {})[args] = result = orig_function.bind(self).call(*args)
-              opts[:freeze] and result.freeze
-              $DEBUG and warn "#{self.class} cached function #{function_id}(#{args.inspect unless args.empty?}) = #{result.inspect}"
             end
-            result
           end
         end
         function_ids.size == 1 ? function_ids.first : function_ids
