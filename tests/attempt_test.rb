b18449cdaef81a1cca11279219ae625a15e368df
Florian Frank <flori@ping.de>
2015-11-09 14:11:05 +0100

    Exponential retries feature added

--- tests/attempt_test.rb
+++ tests/attempt_test.rb
@@ -3,31 +3,33 @@ require 'tins/xt'
 
 module Tins
   class AttemptTest < Test::Unit::TestCase
+    class MyError < StandardError; end
+
+    class MyException < Exception; end
+
+    def test_attempt_with_invalid_arguments
+      assert_raise(ArgumentError) { attempt(:attempts => 0) {  } }
+    end
 
     def test_attempt_block_condition
       assert attempt(:attempts => 1, :exception_class => nil) { |c| c == 1 }
       assert attempt(:attempts => 3, :exception_class => nil) { |c| c == 1 }
       assert_equal false, attempt(:attempts => 3, :exception_class => nil) { |c| c == 4 }
-      assert_nil attempt(:attempts => 0, :exception_class => nil) { |c| c == 4 }
-      assert_raise(Exception) { attempt(:attempts => 3, :exception_class => nil) { raise Exception } }
+      assert_raise(MyException) { attempt(:attempts => 3, :exception_class => nil) { raise MyException } }
     end
-
-    class MyError < StandardError; end
-    class MyException < Exception; end
 
     def test_attempt_default_exception
       assert attempt(1) { |c| c != 1 and raise MyError }
       assert attempt(3) { |c| c != 1 and raise MyError }
       assert_equal false, attempt(3) { |c| c != 4 and raise MyError }
-      assert_nil attempt(0) { |c| c != 4 and raise MyError }
       assert_raise(Exception) { attempt(3) { raise Exception } }
     end
 
     def test_attempt_exception
-      assert attempt(:attempts => 1, :exception_class => MyException) { |c| c != 1 and raise MyException }
-      assert attempt(:attempts => 3, :exception_class => MyException) { |c| c != 1 and raise MyException }
-      assert_nil attempt(:attempts => 0, :exception_class => MyException) { |c| c != 4 and raise MyException }
-      assert_raise(Exception) { attempt(:attempts => 3, :exception_class => MyException) { raise Exception } }
+      assert_equal false,
+        attempt(:attempts => 3, :exception_class => MyException) { raise MyException }
+      assert_equal true,
+        attempt(:attempts => 3, :exception_class => MyException) { |c| c == 1 and raise MyException }
     end
 
     def test_reraise_exception
