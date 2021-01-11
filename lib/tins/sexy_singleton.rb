4edc1b26b480ec42abe5a38f77c8a13dba61ad61
Florian Frank <flori@ping.de>
2021-01-11 14:59:34 +0100

    Handle ruby 3.0 keyword arguments

--- lib/tins/sexy_singleton.rb
+++ lib/tins/sexy_singleton.rb
@@ -12,28 +12,54 @@ module Tins
   class << SexySingleton
     alias __old_singleton_included__ included
 
-    def included(klass)
-      __old_singleton_included__(klass)
-      (class << klass; self; end).class_eval do
-        if Object.method_defined?(:respond_to_missing?)
-          def  respond_to_missing?(name, *args)
-            instance.respond_to?(name) || super
-          end
-        else
-          def respond_to?(name, *args)
-            instance.respond_to?(name) || super
-          end
-        end
-
-        def method_missing(name, *args, &block)
-          if instance.respond_to?(name)
-            instance.__send__(name, *args, &block)
+    if RUBY_VERSION < "2.7"
+      def included(klass)
+        __old_singleton_included__(klass)
+        (class << klass; self; end).class_eval do
+          if Object.method_defined?(:respond_to_missing?)
+            def  respond_to_missing?(name, *args)
+              instance.respond_to?(name) || super
+            end
           else
-            super
+            def respond_to?(name, *args)
+              instance.respond_to?(name) || super
+            end
+          end
+
+          def method_missing(name, *args, &block)
+            if instance.respond_to?(name)
+              instance.__send__(name, *args, &block)
+            else
+              super
+            end
           end
         end
+        super
       end
-      super
+    else
+      def included(klass)
+        __old_singleton_included__(klass)
+        (class << klass; self; end).class_eval do
+          if Object.method_defined?(:respond_to_missing?)
+            def  respond_to_missing?(name, *args, **kwargs)
+              instance.respond_to?(name) || super
+            end
+          else
+            def respond_to?(name, *args, **kwargs)
+              instance.respond_to?(name) || super
+            end
+          end
+
+          def method_missing(name, *args, **kwargs, &block)
+            if instance.respond_to?(name)
+              instance.__send__(name, *args, &block)
+            else
+              super
+            end
+          end
+        end
+        super
+      end
     end
   end
 end
