c95edfc5de5dbac85fcaf9095557fa14d5a0c39d
Florian Frank <flori@ping.de>
2022-11-25 00:55:11 +0100

    test process convenience method

--- tests/limited_test.rb
+++ tests/limited_test.rb
@@ -18,5 +18,21 @@ module Tins
       assert_equal 5, count.keys.uniq.size
       assert_equal 5, limited.wait.size
     end
+
+    def test_process
+      count = {}
+      Tins::Limited.new(5).process do |limited|
+        5.times do
+          limited.execute do
+            count[Thread.current] = true
+            sleep 1
+          end
+        end
+        until count.size >= 5
+          sleep 0.1
+        end
+        assert_equal 5, count.keys.uniq.size
+      end
+    end
   end
 end
