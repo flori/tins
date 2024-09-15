dcbdc3dab16aea0fbd8a516c32e93eb414c746d4
Florian Frank <flori@ping.de>
2024-09-15 12:06:49 +0200

    Refactor deprecation logic and tests
    - Update `Tins::Deprecate#deprecate` method to allow for optional
      `new_method` parameter.
    - Modify `tests/deprecate_test.rb` to test deprecated methods with and
      without messages.

--- lib/tins/deprecate.rb
+++ lib/tins/deprecate.rb
@@ -1,11 +1,11 @@
 module Tins
   module Deprecate
-    def deprecate(method:, new_method:, msg: nil)
-      msg ||= "[DEPRECATION] `%{method}` is deprecated. Please use `%{new_method}` instead."
-      msg = msg % { method: method, new_method: new_method }
+    def deprecate(method:, new_method: nil, message: nil)
+      message ||= '[DEPRECATION] `%{method}` is deprecated. Please use `%{new_method}` instead.'
+      message = message % { method: method, new_method: new_method }
       m = Module.new do
         define_method(method) do |*a, **kw, &b|
-          warn msg
+          warn message
           super(*a, **kw, &b)
         end
       end
--- tests/deprecate_test.rb
+++ tests/deprecate_test.rb
@@ -6,8 +6,8 @@ module Tins
     class A < Array
       attr_reader :warned
 
-      def warn(msg)
-        @warned = msg =~ /DEPRECATION/
+      def warn(message)
+        @warned = message if message =~ /DEPRECATION/
       end
 
       extend Tins::Deprecate
@@ -16,13 +16,27 @@ module Tins
           to_a
         end,
         new_method: :to_a
+
+      extend Tins::Deprecate
+      deprecate method:
+        def zum_vektor
+          to_a
+        end,
+        message: '[DEPRECATION] method `%{method}` is obsolete.'
     end
 
     def test_deprecate
       a = A[ 1, 2 ]
       assert_nil a.warned
-      assert_equal a.zum_felde, [ 1, 2 ]
-      assert a.warned
+      assert_equal [ 1, 2 ], a.zum_felde
+      assert_match(/zum_felde` is deprecated/, a.warned)
+    end
+
+    def test_deprecate_with_message
+      a = A[ 1, 2 ]
+      assert_nil a.warned
+      assert_equal [ 1, 2 ], a.zum_vektor
+      assert_match(/zum_vektor` is obsolete/, a.warned)
     end
   end
 end
