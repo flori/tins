daf6afa0d7c1741ef1bcefd94a7214fe29fe7722
Florian Frank <flori@ping.de>
2025-08-18 15:30:37 +0200

    Update test helper to use GemHadar::SimpleCov instead of manual SimpleCov setup
    - Replace `SimpleCov.start` with `GemHadar::SimpleCov.start`

--- tests/test_helper.rb
+++ tests/test_helper.rb
@@ -1,9 +1,5 @@
-if ENV['START_SIMPLECOV'].to_i == 1
-  require 'simplecov'
-  SimpleCov.start do
-    add_filter "#{File.basename(File.dirname(__FILE__))}/"
-  end
-end
+require 'gem_hadar/simplecov'
+GemHadar::SimpleCov.start
 begin
   require 'debug'
 rescue LoadError
