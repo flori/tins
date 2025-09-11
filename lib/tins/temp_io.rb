1b1a9a79176d8d981cfd1bac8f5c07ace1a92eda
Florian Frank <flori@ping.de>
2025-09-11 10:51:20 +0200

    Add documentation comments to TempIO module and Enum class

--- lib/tins/temp_io.rb
+++ lib/tins/temp_io.rb
@@ -1,7 +1,14 @@
 require 'tmpdir'
 
 module Tins
+  # A module for creating temporary files and handling their contents securely.
   module TempIO
+    # Creates a temporary file with the given content and yields it to a block.
+    #
+    # @param content [String, #call] the content to write to the temporary file
+    # @param name [String, Symbol] the base name for the temporary file
+    # @yield [io] yields the temporary file handle to the given block
+    # @return [Object] the return value of the block
     def temp_io(content: nil, name: __method__)
       content.nil? and raise ArgumentError, "missing keyword: content"
       name = File.basename(name.to_s)
