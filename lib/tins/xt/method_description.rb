d1f8e917e34ecd0b81d75d6defbd117c2bcd1bb3
Florian Frank <flori@ping.de>
2025-09-11 11:03:24 +0200

    Remove unused xt modules and remove method string representation
    - Remove `tins/xt/count_by` require from `lib/tins/xt.rb`
    - Remove `tins/xt/uniq_by` require from `lib/tins/xt.rb`
    - Remove `tins/xt/deep_const_get` require from `lib/tins/xt.rb`
    - Remove redundant `to_s` alias and `inspect` method overrides in
      `MethodDescription` module
    - The `MethodDescription` class no longer overrides `to_s` or `inspect`
      methods

--- lib/tins/xt/method_description.rb
+++ lib/tins/xt/method_description.rb
@@ -3,21 +3,9 @@ require 'tins/method_description'
 module Tins
   class ::UnboundMethod
     include MethodDescription
-
-    alias to_s description
-
-    def inspect
-      "#<#{self.class}: #{description}>"
-    end
   end
 
   class ::Method
     include MethodDescription
-
-    alias to_s description
-
-    def inspect
-      "#<#{self.class}: #{description}>"
-    end
   end
 end
