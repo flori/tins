0b9cd99cfa4d29611eeb7a3b36cb04dec3d1ebc1
Florian Frank <flori@ping.de>
2025-09-11 10:19:49 +0200

    Add comprehensive documentation to Bijection class

--- lib/tins/bijection.rb
+++ lib/tins/bijection.rb
@@ -1,5 +1,12 @@
 module Tins
+  # A hash subclass that ensures bijection between keys and values
   class Bijection < Hash
+    # Creates a new Bijection instance with key-value pairs.
+    #
+    # @param pairs [Array] an array of key-value pairs to populate the
+    # bijection
+    #
+    # @return [Bijection] a new bijection populated with the provided pairs
     def self.[](*pairs)
       pairs.size % 2 == 0 or
         raise ArgumentError, "odd number of arguments for #{self}"
@@ -15,10 +22,20 @@ module Tins
       end
     end
 
+    # The initialize method sets up a new instance with an inverted bijection.
+    #
+    # @param inverted [ Bijection ] the inverted bijection object, defaults to
+    # a new Bijection instance
     def initialize(inverted = Bijection.new(self))
       @inverted = inverted
     end
 
+    # The fill method populates the object with content from a block if it is
+    # empty.
+    #
+    # @return [ self ] returns the object itself after filling or if it was not
+    # empty
+    # @yield [ self ] yields self to the block for population
     def fill
       if empty?
         yield self
@@ -27,6 +44,9 @@ module Tins
       self
     end
 
+    # The freeze method freezes the current object and its inverted attribute.
+    #
+    # @return [Object] the frozen object
     def freeze
       r = super
       unless @inverted.frozen?
@@ -35,12 +55,26 @@ module Tins
       r
     end
 
+    # The []= method assigns a value to a key in the hash and maintains an
+    # inverted index.
+    #
+    # @param key [ Object ] the key to assign
+    # @param value [ Object ] the value to assign
+    #
+    # @return [ Object ] the assigned value
+    #
+    # @note This method will not overwrite existing keys, it will return early
+    # if the key already exists.
     def []=(key, value)
       key?(key) and return
       super
       @inverted[value] = key
     end
 
+    # The inverted attribute returns the inverted state of the object.
+    # Creates a new Bijection instance filled with key-value pairs.
+    #
+    # @return [Bijection] a new bijection instance with the specified key-value pairs
     attr_reader :inverted
   end
 end
