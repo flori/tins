bbad018e1c9d2f130ed551c8e274cab310d275db
Florian Frank <flori@ping.de>
2025-09-11 10:13:58 +0200

    Update YARD documentation guidelines and remove test file context
    - Remove `tests` namespace and related file inclusion logic
    - Correct typo in `guidelins` to `guidelines`
    - Modify guidelines for initialize method documentation
    - Add rule that @return should never be used in Ruby method comments
    - Emphasize that every Ruby method returns something and @return should
      be omitted when unsure or for side effects

--- .contexts/code_comment.rb
+++ .contexts/code_comment.rb
@@ -5,22 +5,19 @@ context do
     end
   end
 
-  namespace "tests" do
-    Dir['tests/**/*.rb'].each do |filename|
-      file filename, tags: 'test'
-    end
-  end
-
 
   file 'README.md', tags: 'documentation'
 
   file '.contexts/yard.md', tags: [ 'yard', 'cheatsheet' ]
 
-  meta guidelins: <<~EOT
+  meta guidelines: <<~EOT
     # Guidelines for creating YARD documentation
 
     - Look into the file, with tags yard and cheatsheet for how comment ruby
       constructs.
-    - In comments above initialize methods never omit @return
+    - In comments above initialize methods **ALWAYS** omit @return.
+    - **NEVER** output @return [ void ] in comments of any method, because
+      in Ruby every method returns something. If you don't know or if the
+      method is just called because its side effect just omit the @return.
   EOT
 end
