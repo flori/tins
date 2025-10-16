db0f8aa4c3b355ca5e851bc9d711bf21e1e212a0
Florian Frank <flori@ping.de>
2025-10-16 21:53:50 +0200

    Update version to **1.45.0** across all relevant files
    - Updated `VERSION` file to reflect new version **1.45.0**
    - Modified `lib/tins/version.rb` to set `VERSION` constant to **1.45.0**
    - Changed gemspec version from **1.44.1** to **1.45.0** in `tins.gemspec` file

--- lib/tins/version.rb
+++ lib/tins/version.rb
@@ -1,6 +1,6 @@
 module Tins
   # Tins version
-  VERSION         = '1.44.1'
+  VERSION         = '1.45.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
