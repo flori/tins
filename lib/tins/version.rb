299c8d464ceac0b958580924d03cea586a78a4a1
Florian Frank <flori@ping.de>
2025-12-20 01:57:47 +0100

    Update version to **1.51.0**
    - Updated `VERSION` file to reflect new version **1.51.0**
    - Changed `Tins::VERSION` constant in `lib/tins/version.rb` from
      **1.50.0** to **1.51.0**
    - Modified gemspec file to set version to **1.51.0** in both stub and
      specification blocks

--- lib/tins/version.rb
+++ lib/tins/version.rb
@@ -1,6 +1,6 @@
 module Tins
   # Tins version
-  VERSION         = '1.50.0'
+  VERSION         = '1.51.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
