d1223f5e83ffc0503d90541e478550c055a317b7
Florian Frank <flori@ping.de>
2026-04-14 21:04:53 +0200

    Bump version to **1.53.0**
    - Updated `VERSION` file to reflect new version **1.53.0**
    - Changed `Tins::VERSION` constant in `lib/tins/version.rb` to **1.53.0**
    - Updated gemspec version metadata to **1.53.0** in `tins.gemspec` file

--- lib/tins/version.rb
+++ lib/tins/version.rb
@@ -1,6 +1,6 @@
 module Tins
   # Tins version
-  VERSION         = '1.52.0'
+  VERSION         = '1.53.0'
   VERSION_ARRAY   = VERSION.split('.').map(&:to_i) # :nodoc:
   VERSION_MAJOR   = VERSION_ARRAY[0] # :nodoc:
   VERSION_MINOR   = VERSION_ARRAY[1] # :nodoc:
